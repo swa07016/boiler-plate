e86e609b18103520a5fa620d4df0a8c4cc5d4133
swa07016 <swa07016@naver.com>
2020-05-02 15:53:15 +0900

    Password Encryption

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
 
 const userSchema = mongoose.Schema({
     name: {
@@ -26,6 +28,33 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+userSchema.pre('save', function (next) {
+    var user = this;
+    if (user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if (err) return next(err)
+
+            bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next()
+    }
+})
+
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+    //plainPassword 1234567    암호화된 비밀번호 $2b$10$tXMGpSJeAPwgpmLxIkYVwuAORGH7hq4hjQt3RycH9asFGwQXcqcbe
+    bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+        if (err) return cb(err),
+            cb(null, isMatch)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
