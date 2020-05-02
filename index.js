ceffa75e06f1a94e3f25cf436568bb82092e6b08
swa07016 <swa07016@naver.com>
2020-05-02 17:45:43 +0900

    Auth

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const config = require('./config/key');
+const { auth } = require('./middleware/auth');
 const { User } = require('./models/User');
 
 // application/x-www-form-urlencoded
@@ -21,7 +22,7 @@ mongoose.connect( config.mongoURI , {
 
 app.get('/', (req, res) => res.send('hello world'))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     const user = new User(req.body)
     user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err })
@@ -66,5 +67,22 @@ app.post('/api/users/login', (req, res) => {
     })
   })
   
+
+// role 1 어드민    role 2 특정 부서 어드민 
+// role 0 -> 일반유저   role 0이 아니면  관리자 
+  app.get('/api/users/auth', auth, (req, res) => {
+    //여기 까지 미들웨어를 통과해 왔다는 얘기는  Authentication 이 True 라는 말.
+    res.status(200).json({
+      _id: req.user._id,
+      isAdmin: req.user.role === 0 ? false : true,
+      isAuth: true,
+      email: req.user.email,
+      name: req.user.name,
+      lastname: req.user.lastname,
+      role: req.user.role,
+      image: req.user.image
+    })
+  })
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
