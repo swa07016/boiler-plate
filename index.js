ab1ca04b8327a6127acd1e03c365767b04edada8
swa07016 <swa07016@naver.com>
2020-05-02 17:28:28 +0900

    Login

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const port = 5000;
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 const { User } = require('./models/User');
 
@@ -9,6 +10,7 @@ const { User } = require('./models/User');
 app.use(bodyParser.urlencoded({ extended: true }));
 // application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose');
 mongoose.connect( config.mongoURI , {
@@ -18,6 +20,7 @@ mongoose.connect( config.mongoURI , {
 .catch(err => console.log(err))  
 
 app.get('/', (req, res) => res.send('hello world'))
+
 app.post('/register', (req, res) => {
     const user = new User(req.body)
     user.save((err, userInfo) => {
@@ -25,5 +28,43 @@ app.post('/register', (req, res) => {
         return res.status(200).json({ success:true })
     })
 })
+
+
+app.post('/api/users/login', (req, res) => {
+
+    // console.log('ping')
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }, (err, user) => {
+  
+      // console.log('user', user)
+      if (!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "제공된 이메일에 해당하는 유저가 없습니다."
+        })
+      }
+  
+      //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+      user.comparePassword(req.body.password, (err, isMatch) => {
+        // console.log('err',err)
+  
+        // console.log('isMatch',isMatch)
+  
+        if (!isMatch)
+          return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+  
+        //비밀번호 까지 맞다면 토큰을 생성하기.
+        user.generateToken((err, user) => {
+          if (err) return res.status(400).send(err);
+  
+          // 토큰을 저장한다.  어디에 ?  쿠키 , 로컳스토리지 
+          res.cookie("x_auth", user.token)
+            .status(200)
+            .json({ loginSuccess: true, userId: user._id })
+        })
+      })
+    })
+  })
+  
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
