a770df0344a272ebeb5448519b7fd5565241170a
swa07016 <swa07016@naver.com>
2020-05-06 11:54:23 +0900

    Login Page Added

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -6,12 +6,14 @@ function LandingPage() {
     useEffect(() => {
         axios.get('/api/hello')
         .then(response=> { console.log(response)})
-
      }, [])
 
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            <h2>시작 페이지</h2>
         </div>
     )
 }
